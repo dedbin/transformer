8cfc21d5691dae6ea81a237c96526e58f949af89
dedbin <black13dog@yandex.ru>
2023-08-14 14:03:48 +0300

    transformer block done

--- model.py
+++ model.py
@@ -46,3 +46,40 @@ class MultiHeadAttention(nn.Module):
         out = torch.cat([att(x) for att in self.heads], dim=-1)
         out = self.dropout(self.proj(out))
         return out
+
+
+class FeedForward(nn.Module):
+    """A simple linear layer followed by ReLu"""
+
+    def __init__(self, num_embed, dropout):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(num_embed, 4 * num_embed),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(4 * num_embed, num_embed),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class TransformerBlock(nn.Module):
+    """Transformer block. This class will group Multihead and FeedForward layers."""
+
+    def __init__(self, num_heads, block_size, num_embed, dropout):
+        super().__init__()
+        head_size = block_size // num_heads
+        self.sa = MultiHeadAttention(
+            num_heads, head_size, num_embed, block_size, dropout
+        )
+        self.ff = FeedForward(num_embed, dropout)
+        self.norm1 = nn.LayerNorm(num_embed)
+        self.norm2 = nn.LayerNorm(num_embed)
+
+    def forward(self, x):
+        x = x + self.sa(self.norm1(x))
+        x = x + self.ff(self.norm2(x))
+        return x
+
