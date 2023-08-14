e43c4b577bad21bf7adf37ff02ae1d36287b6648
dedbin <black13dog@yandex.ru>
2023-08-14 13:28:02 +0300

    Multi head attention done

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from utils import *
 
 class AttentionHead(nn.Module):
     """One head of self-attention."""
+
     def __init__(self, head_size, num_embed, block_size, dropout):
         super().__init__()
         self.key = nn.Linear(num_embed, head_size, bias=False)
@@ -19,10 +20,29 @@ class AttentionHead(nn.Module):
         k = self.key(x)
         q = self.query(x)
         v = self.value(x)
-        wei = q @ k.transpose(-2, -1) * C**-0.5
+        wei = q @ k.transpose(-2, -1) * C ** -0.5
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
         wei = F.softmax(wei, dim=-1)
         wei = self.dropout(wei)
         v = self.value(x)
         out = wei @ v
+        return out
+
+
+class MultiHeadAttention(nn.Module):
+    """Multi-head attention."""
+
+    def __init__(self, num_heads, head_size, num_embed, block_size, dropout):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [AttentionHead(
+                head_size, num_embed, block_size, dropout
+            ) for _ in range(num_heads)]
+        )
+        self.proj = nn.Linear(num_heads * head_size, num_embed, bias=False)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([att(x) for att in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
         return out
